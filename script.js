6ac63c3b331a2b1571206a6b96dda5ec48908345
VerityDuan <verityduan@gmail.com>
2024-11-15 15:14:20 -0700

    .

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 
 
-
-
-
+document.getElementById("changeTextBtn").onclick = function() {
+    document.getElementById("greeting").innerHTML = "Hello, JavaScript!";}
+    
 const footer = document.getElementById('footer');
 const currentYear = new Date().getFullYear();
 footer.innerHTML = `(c) Your Name, ${currentYear}`;
