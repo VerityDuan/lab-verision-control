3b07e3efdc1f958fc94e76338e63a223de5b016b
VerityDuan <verityduan@gmail.com>
2024-11-15 16:33:42 -0700

    changing

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 
+let emoji = document.getElementById("emoji");
+
+let positionX = 0;  
+let positionY = 0; 
+
+let speed = 5; 
+
+function moveEmoji() {
+    positionX += speed;
+    positionY += speed;
+
+    emoji.style.position = "absolute"; 
+    emoji.style.left = positionX + "px"; 
+    emoji.style.top = positionY + "px";  
+
+    if (positionX > window.innerWidth || positionY > window.innerHeight) {
+        positionX = 0;
+        positionY = 0;
+    }
+}
+setInterval(moveEmoji, 10);
 
 
 document.getElementById("changeTextBtn").onclick = function() {
