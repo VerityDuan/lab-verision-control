bec20d5078d30e2587cf63670e5ca1fa1c657049
VerityDuan <verityduan@gmail.com>
2024-11-16 11:32:11 -0700

    .

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-
-let emoji = document.getElementById("emoji");
-
-let positionX = 0;  
-let positionY = 0; 
-
-let speed = 5; 
-
-function moveEmoji() {
-    positionX += speed;
-    positionY += speed;
-
-    emoji.style.position = "absolute"; 
-    emoji.style.left = positionX + "px"; 
-    emoji.style.top = positionY + "px";  
-
-    if (positionX > window.innerWidth || positionY > window.innerHeight) {
-        positionX = 0;
-        positionY = 0;
-    }
+function revealMessage (){
+    document.getElementById ("hiddenMessage").style.display= "block";
 }
-setInterval(moveEmoji, 10);
-
-
-document.getElementById("changeTextBtn").onclick = function() {
-    document.getElementById("greeting").innerHTML = "Hello, JavaScript!";}
-    
-const footer = document.getElementById('footer');
-const currentYear = new Date().getFullYear();
-footer.innerHTML = `(c) Your Name, ${currentYear}`;
-
