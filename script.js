93ec645cc0bcdbc97f750a46e0ec8fbb3a9dfd80
VerityDuan <verityduan@gmail.com>
2024-11-16 15:47:57 -0700

    .

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
+document.addEventListener("DOMContentLoaded"), ()=> {
 
 const today= new Date ();
 const year = today.getFullYear ();
-const fullDate= today.toDateString ();
-
-document.getElementById("adddate").innerHTML= "All Rights Reserved, Verity Duan 2024";
-
-const element= document.getElementById ("adddate");
-console.log ("element found:", element);
 
 
-
+const footerElement= document.getElementById ("adddate");
+if (footerElement){
+    footerElement.innerHTML= `All Rights Reserved, Verity Duan ${year}`;
+} else {
+    console.error("Element with ID 'adddate' not found.");
+}
 
 
 function revealMessage(){
-    document.getElementById ("hiddenMessage").style.display= "block";
+    const hiddenMessageElement = document.getElementById ("hiddenMessage");
+    if (hiddenMessageElement){
+        hiddenMessageElement.style.display="block"
+    } else {
+        console.error("Element with ID 'hiddenMessage' not found.")
+    }
+}
+
+function getId(btn){
+    const alert=document.getElementById ("b")
+    
+    
 }
 
 
@@ -25,3 +36,5 @@ console.log(typeof firstName);
 console.log(`your name is ${firstName}`)
 console.log(`you like ${favoriteFood}`)
 console.log(`your email is ${email}`) 
+}
+
