bde4518b400f29720252d5ccae4f53cafb34c9c4
VerityDuan <verityduan@gmail.com>
2024-11-17 10:57:39 -0700

    .

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-document.addEventListener("DOMContentLoaded"), ()=> {
+document.addEventListener("DOMContentLoaded",()=> {
 
 const today= new Date ();
 const year = today.getFullYear ();
-
+console.log(today)
 
 const footerElement= document.getElementById ("adddate");
 if (footerElement){
     footerElement.innerHTML= `All Rights Reserved, Verity Duan ${year}`;
 } else {
     console.error("Element with ID 'adddate' not found.");
+}
+
+const currentTime= new Date();
+const hours=currentTime.getHours();
+const greetingElement=document.getElementById("greeting");
+if (hours< 12) {
+    greetingElement.innerText= "GoodMorning";
+    greetingElement.classList.add("morning");
+}
+else if (hours >=12&& hours <17){
+    greetingElement.innerText="GoodAfternoon!";
+    greetingElement.classList.add("afternoon");
+} else{
+    greetingElement.innerText="GoodEvening";
+    greetingElement.classList.add ("evening")
 }
 
 
@@ -21,11 +36,35 @@ function revealMessage(){
     }
 }
 
-function getId(btn){
-    const alert=document.getElementById ("b")
-    
-    
+const numbersList= document.getElementById("numbers");
+for (let i=1; i<=12; i++) {
+    const listItem=document.createElement("li");
+
+    console.log(`Creating list item for index ${i}`);
+    if(i%2===0){
+        listItem.textContent= "even";
+    } else{
+        listItem.textContent="odd";
+    }
+    numbersList.appendChild(listItem);
 }
+
+const button = document.getElementById('btn-alert');
+button.addEventListener('click', function() {
+alert('Hello world!');
+});
+function getId(btn){
+    const alert=document.getElementById ("b") 
+};
+
+const alertButton= document.getElementById("alertButton");
+alertButton.onmouseover=function() {
+    alertButton.innerText="YOUSUCK! Just kidding";
+};
+
+alertButton.onmouseout= function() {
+    alertButton.innerText= "Iamthebest"
+};
 
 
 let firstName = "me";
@@ -36,5 +75,5 @@ console.log(typeof firstName);
 console.log(`your name is ${firstName}`)
 console.log(`you like ${favoriteFood}`)
 console.log(`your email is ${email}`) 
-}
+})
 
